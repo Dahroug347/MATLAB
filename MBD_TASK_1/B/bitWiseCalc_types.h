ce284bc4c06c2d09cc309bf0ac0afcdf3cb6c5a1
Dahroug347 <53479779+Dahroug347@users.noreply.github.com>
2022-04-06 14:21:12 +0200

    Add files via upload

---
+++ MBD_TASK_1/B/bitWiseCalc_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: bitWiseCalc_types.h
+ *
+ * Code generated for Simulink model 'bitWiseCalc'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+ * C/C++ source code generated on : Sat Apr  2 16:16:53 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_bitWiseCalc_types_h_
+#define RTW_HEADER_bitWiseCalc_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_bitWiseCalc_T RT_MODEL_bitWiseCalc_T;
+
+#endif                                 /* RTW_HEADER_bitWiseCalc_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
