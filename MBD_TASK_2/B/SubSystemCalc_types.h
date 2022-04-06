7ffee4337c85ca9de41c80d7076f3e8152c0144d
Dahroug347 <53479779+Dahroug347@users.noreply.github.com>
2022-04-06 14:22:38 +0200

    Add files via upload

---
+++ MBD_TASK_2/B/SubSystemCalc_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: SubSystemCalc_types.h
+ *
+ * Code generated for Simulink model 'SubSystemCalc'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+ * C/C++ source code generated on : Sat Apr  2 18:13:12 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_SubSystemCalc_types_h_
+#define RTW_HEADER_SubSystemCalc_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_SubSystemCalc_T RT_MODEL_SubSystemCalc_T;
+
+#endif                                 /* RTW_HEADER_SubSystemCalc_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
